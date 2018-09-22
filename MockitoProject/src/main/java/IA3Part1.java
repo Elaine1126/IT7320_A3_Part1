c8400aa16413e5cebea192dc55531c66e4cbd18f
demounit <demounit@192.168.1.240>
2018-09-22 21:28:40 +1200

    create interface

---
+++ MockitoProject/src/main/java/IA3Part1.java
@@ -0,0 +1,10 @@
+
+public interface IA3Part1 {
+	public double computeTax (double amount);
+	public double weeklySalary (Employee income);
+	public double Salaryfortnight (Employee income);
+	public double computeKiwiSaver (Employee KiwiOption);
+	
+	
+
+}
